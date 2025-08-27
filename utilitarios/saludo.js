da63d2ecfb9eeba563fedf4f20366f47e5fb5949
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-08-27 10:34:50 -0500

    Se separo las funciones a archivo utlitarios

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -9,36 +9,3 @@ saludar=function(){
     mostrarTxtEnCaja("idNombre","")
 
 }
-
-mostrarTxtEnCaja=function(idComponente, mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.value=mensaje
-}
-
-mostrarImg=function(idComponente, newImg){
-    let componente=document.getElementById(idComponente);
-    componente.src=newImg;
-}
-
-mostrarTexto=function(idComponente, mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
-
-recuperarTexto=function(idComponente){
-    let valorCaja=document.getElementById(idComponente);
-    let txt=valorCaja.value
-    return txt
-}
-
-recuperarInt=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorEntero=parseInt(valorCaja);
-    return valorEntero
-}
-
-recuperarFloat=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente)
-    let valorFlotante=parseFloat(valorCaja) 
-    return valorFlotante
-}
