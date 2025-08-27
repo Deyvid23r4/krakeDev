cd833b6ffb9fc3c83cfd145339baf49a7caf63b5
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-08-27 15:43:20 -0500

    calcula y muestra el iva en pantalla

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -47,7 +47,9 @@ calcularValorTotal = function () {
         Si el caso de prueba es exitoso, hacer un commit
      */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
+    valorIVA=calcularIva(valorSubtotal-valorDescuento)
     // El IVA debe calcularse sobre el valor del subtotal menos el descuento
+    mostrarTexto("lblValorIVA", valorIVA)
     //9. Mostrar el resultado en el componente lblValorIVA    
         /*
             Caso de prueba: 
