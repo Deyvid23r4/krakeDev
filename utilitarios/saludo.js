509bb5897a27ab84495abba5c5a3a3008254bfd0
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-08-27 08:31:00 -0500

    recupera int y float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,11 +1,25 @@
 saludar=function(){
     let nombre=recuperarTexto("idNombre");
     let apellido=recuperarTexto("idApellido");
-    console.log (nombre, apellido)
+    let edad=recuperarInt("idEdad")
+    let estatura=recuperarFloat("idEstatura")
+    console.log ("hola "+nombre, apellido, edad, estatura)
 }
 
-recuperarTexto=function(id){
-    let nombre=document.getElementById(id);
-    nombre=nombre.value
-    return nombre
+recuperarTexto=function(idComponente){
+    let valorCaja=document.getElementById(idComponente);
+    let txt=valorCaja.value
+    return txt
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero
+}
+
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente)
+    let valorFlotante=parseFloat(valorCaja) 
+    return valorFlotante
 }
