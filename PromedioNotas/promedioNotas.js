c796d4d53fbd71131b71bfd1872470d59417ab64
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-09-01 11:00:15 -0500

    v2 promedio notas

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -5,10 +5,16 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(n1,n2,n3);
     promedio=promedio.toFixed(2);
     console.log(promedio)
-    if(promedio>7){
-        mostrarImagen("idImg", "exelente.gif")
+    if(promedio>0 && promedio<5){
+        mostrarImagen("idImg", "./img/reprobado.gif")
+    }
+    else if(promedio>=5 && promedio<=8){
+        mostrarImagen("idImg", "./img/buentrabajo.gif")
+    }
+    else if(promedio>8 && promedio<=10){
+        mostrarImagen("idImg", "./img/exelente.gif")
     }
     else{
-        mostrarImagen("idImg", "fail.gif")
+        mostrarImagen("idImg", "./img/maldatos.webp")
     }
 }
