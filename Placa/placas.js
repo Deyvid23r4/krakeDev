3f319548912bb7541536e5b6f89acd21366f9cd4
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-09-09 00:18:38 -0500

    Validacion de provincia

--- Placa/placas.js
+++ Placa/placas.js
@@ -1,11 +1,18 @@
 validarPlaca = function () {
     let placa = recuperarTexto("lblPlaca")
     let errorEstructura = validarEstructura(placa)
+    let provincia = obtenerProvincia(placa)
     if (errorEstructura === null) {
         mostrarTexto("lblValidar", " VALIDA")
         mostrarTexto("lblMensaje", "NINGUNO")
+        if (provincia != null) {
+            mostrarTexto("lblProvincia", provincia)
+        } else {
+            mostrarTexto("lblProvincia", "PROVINCIA INCORRECTA")
+        }
     } else {
         mostrarTexto("lblValidar", " INVALIDA")
         mostrarTexto("lblMensaje", errorEstructura)
+        mostrarTexto("lblProvincia", "")
     }
 }
