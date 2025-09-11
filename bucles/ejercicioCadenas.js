74f9245b0fcef207d8dffee8771cd50606644cb2
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-09-11 13:56:13 -0500

    Invertir cadena

---
+++ bucles/ejercicioCadenas.js
@@ -0,0 +1,39 @@
+ejecutarPrueba1 = function () {
+    let mensaje;
+    mensaje = recuperarTexto("lblCadena")
+    recorrerCadena(mensaje)
+}
+
+recorrerCadena = function (cadena) {
+    //0123456
+    //Juanito
+    let caracter;
+
+    for (let posicion = 0; posicion < cadena.length; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("Caracter " + caracter + " posicion " + posicion);
+    }
+
+    for (let posicion = 0; posicion < cadena.length - 1; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("CARACTER " + caracter + " posicion " + posicion);
+    }
+}
+
+
+
+ejecutarInvertir = function () {
+    let mensaje = recuperarTexto("lblCadena")
+    invertirCadena(mensaje)
+}
+
+
+invertirCadena = function (cadena) {
+    let caracter;
+    let resultado = ""
+    for (let posicion = cadena.length; posicion >= 0; posicion--) {
+        caracter = cadena.charAt(posicion);
+        resultado = resultado + caracter
+        mostrarTexto("lblInverso", resultado)
+    }
+}
